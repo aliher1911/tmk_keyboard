fa64b25a0a29ac5d7d993957846f41e3b460cdf6
Oleg Afanasiev <oafanasiev@gmail.com>
2020-11-05 10:30:58 +0000

    Fixed capslock led to use PJRC built in

--- keyboard/split/led.c
+++ keyboard/split/led.c
@@ -18,17 +18,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <avr/io.h>
 #include "stdint.h"
 #include "led.h"
+#include "debug.h"
 
+#define LED_BIT 6
 
 void led_set(uint8_t usb_led)
 {
     if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-        // output low
-        DDRB |= (1<<2);
-        PORTB &= ~(1<<2);
+    	dprintln("CL On");
+        DDRD |= (1<<LED_BIT);
+        PORTD |= (1<<LED_BIT);
     } else {
+    	dprintln("CL Off");
         // Hi-Z
-        DDRB &= ~(1<<2);
-        PORTB &= ~(1<<2);
+        DDRD |= (1<<LED_BIT);
+        PORTD &= ~(1<<LED_BIT);
     }
 }
